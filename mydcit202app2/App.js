e28e748d6f457b334e98f0d59c550d8330ca465b
JD-2104 <jdanso019@st.ug.edu.gh>
2024-05-31 18:16:25 +0000

    changes

--- mydcit202app2/App.js
+++ mydcit202app2/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex: 1, backgroundColor: '#f7f0e8'}}>
+      <Text style={{width: 180, height: 37, top: 19, fontWeight: 700, fontSize: 32, lineHeight: 38.4, left: 20}}><b>Hello , Devs</b></Text>
+
+      <Text style={{width: 77, height: 13, top: 29, fontWeight: 500, fontSize: 12, lineHeight: 14.4, left: 20}}>14 task today</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f7f0e8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f7f0e8',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     fontFamily: 'Lato',
+//     fontWeight: 700,
+//     fontSize: 20,
+//     lineHeight: 24,
+//   },
+// });
